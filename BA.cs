b325bd53abdc75c5b144def7db5505461efd903b
kami <65225845+kamikire@users.noreply.github.com>
2021-06-05 20:52:43 +0900

    Add files via upload

---
+++ BA.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BA : MonoBehaviour
+{
+    [SerializeField, Header("Speed"), Range(1, 100)]
+    public float speed = 50f;
+    void Update()
+    {
+        int i;
+        for (i = 1; i <= 2628;)
+        {
+            Sprite[] sprites = Resources.LoadAll<Sprite>("Textures");
+            var index = (int)Mathf.Repeat(Time.frameCount * speed / 100, sprites.Length);
+            var renderer = GetComponent<MeshRenderer>();
+            var material = renderer.material;
+
+            var sprite = sprites[index];
+            var texSize = new Vector2(sprite.texture.width, sprite.texture.height);
+            var rect = sprite.textureRect;
+            material.mainTexture = sprite.texture;
+            material.mainTextureOffset = new Vector2(rect.x / texSize.x, rect.y / texSize.y);
+            material.mainTextureScale = new Vector2(rect.width / texSize.x, rect.height / texSize.y);
+            material.SetTexture("BAMat", sprite.texture);
+            material.SetTextureOffset("BAMat", new Vector2(rect.x, rect.y));
+            material.SetTextureScale("BAMat", new Vector2(1 / rect.width, 1 / rect.height));
+
+            renderer.material = material;
+            return;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+#  Unity BadApple No VideoPlayer(Unity2018)
+ご覧いただきありがとうございます<br>
+このプログラムは、Unity2018上でビデオプレーヤーを使わず、スプライトイメージのみで動画を流すアセットです<br>
+<br>
+Thank you for watching.<br>
+This assets is images Animation program<br>
+## How To Install This Assets?<br>
+まず、最初にこのリポジトリをダウンロードし、解凍します　そのあとプロジェクトフォルダに入れます<br>
+入れるフォルダ（入れるディレクトリ）<br>
+C:\Users\example_user\Documents\hoge\Assets ← 例(ユーザー名は各自で入力してください)<br>
+次に、Resources.7zを解凍ソフトで解凍します<br>
+この時、Resources.7zはAssetsフォルダ直下に配置してください←決められたフォルダに入れないと動画が流れません<br>
+そのあと、Resources/Textures/に入っている画像をShiftで全選択した後、インスペクター上でDefaultからspriteに変更します<br>
+最後に、適当な3Dオブジェクトを作ってからBA.csをくっつけた後、マテリアルBAMatをメッシュレンダラーのマテリアルに設定して終了です<br>
+実行して、動画が流れれば成功です<br>
+<br>
+First, This assets download to unzip file and into Unity project folder.<br>
+Example<br>
+C:\Users\example_user\Documents\hoge\Assets ← into this assets<br>
+Second, unzip Resources.7z<br>
+⚠Resources folder is must be into Assets folder!!!<br>
+Change the BadApple images in the Textures folder from the default to sprite<br>
+Finaly, attach BA.cs to any object and setting BAMat material a 3D object<br>
+## Download<br>
+https://github.com/kamikire/Unity_BadApple_NoVideoPlayer.git
